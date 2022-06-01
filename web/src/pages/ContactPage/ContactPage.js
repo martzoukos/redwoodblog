270f47f2ea5ecea06977aa58d9a845fb3e2071f6
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2022-06-01 23:20:35 +0300

    Finish Chapter 3 - Building a Form

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,63 @@
+import { MetaTags } from '@redwoodjs/web'
+import {
+  FieldError,
+  Form,
+  Label,
+  TextField,
+  TextAreaField,
+  Submit,
+} from '@redwoodjs/forms'
+
+const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <Label errorClassName="error" name="name">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label errorClassName="error" name="email">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label errorClassName="error" name="message">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit>Save</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
